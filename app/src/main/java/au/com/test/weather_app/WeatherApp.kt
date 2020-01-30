0fa317208fe43f0a8eda9bcd41a381975753318b
Hongwei Bai <bhw8412@hotmail.com>
2020-01-31 00:52:41 +1100

    initialize repo

---
+++ app/src/main/java/au/com/test/weather_app/WeatherApp.kt
@@ -0,0 +1,5 @@
+package au.com.test.weather_app
+
+import android.app.Application
+
+class WeatherApp : Application()
---
+++ app/src/main/java/au/com/test/weather_app/home/MainActivity.kt
@@ -0,0 +1,13 @@
+package au.com.test.weather_app.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import au.com.test.weather_app.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
